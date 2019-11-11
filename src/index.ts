c2f0f13a67cfdfc7576955a46e6be94342bd366a
vvvvvvvvvvip <18334795012@163.com>
2019-11-11 15:13:11 +0800

    交互反馈模块完善

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,15 @@ export default {
   getSystemInfoSync,
   createCanvasContext,
   createSelectorQuery,
+  showToast,
+  showLoading,
+  hideLoading,
+  showActionSheet,
 };
 
 /**
  * 警告框
+ * {@link https://ding-doc.dingtalk.com/doc#/dev/ui-feedback 交互反馈}
  */
 export function alert({
   title,
@@ -39,6 +44,7 @@ export function alert({
 
 /**
  * 确认框
+ * {@link https://ding-doc.dingtalk.com/doc#/dev/ui-feedback 交互反馈}
  */
 export function confirm(opt: {
   /** confirm 框的标题 */
@@ -57,6 +63,78 @@ export function confirm(opt: {
       fail: reject
     });
   });
+}
+
+/**
+ * 显示一个弱提示，可选择多少秒之后消失
+ * {@link https://ding-doc.dingtalk.com/doc#/dev/ui-feedback 交互反馈}
+ * @param content 文字内容
+ * @param type toast 类型，展示相应图标，默认 none，支持 success / fail / exception / none。其中 exception 类型必须传文字信息
+ * @param duration 显示时长，单位为 ms，默认 2000。按系统规范，android只有两种(<=2s >2s)
+ */
+export function showToast(opt: {
+  content?: string;
+  type?: string;
+  duration?: number;
+}) {
+  return new Promise((resolve, reject) => {
+    dd.showToast({
+      ...opt,
+      success: resolve,
+      fail: reject,
+    })
+  })
+}
+
+/**
+ * 显示加载提示
+ * {@link https://ding-doc.dingtalk.com/doc#/dev/ui-feedback 交互反馈}
+ * @param content loading的文字内容
+ * @param delay 延迟显示，单位 ms，默认 0。如果在此时间之前调用了 dd.hideLoading 则不会显示
+ */
+export function showLoading(opt: {
+  content?: string;
+  delay?: number;
+}) {
+  return new Promise((resolve, reject) => {
+    dd.showLoading({
+      ...opt,
+      success: resolve,
+      fail: reject,
+    })
+  })
+}
+
+/**
+ * 隐藏加载提示
+ * {@link https://ding-doc.dingtalk.com/doc#/dev/ui-feedback 交互反馈}
+ */
+export function hideLoading() {
+  return dd.hideLoading();
+}
+
+/**
+ * 显示操作菜单
+ * {@link https://ding-doc.dingtalk.com/doc#/dev/ui-feedback 交互反馈}
+ * @param title 菜单标题
+ * @param items 菜单按钮文字数组
+ * @param cancelButtonText 取消按钮文案。注：Android平台此字段无效，不会显示取消按钮
+ */
+export function showActionSheet(opt: {
+  title?: string;
+  items: Array<string>;
+  cancelButtonText?: string;
+}) {
+  return new Promise((resolve, reject) => {
+    dd.showActionSheet({
+      ...opt,
+      success(res: any) {
+        /** @returns index 被点击的按钮的索引，从0开始。点击取消或蒙层时返回 -1 */
+        resolve(res.index);
+      },
+      fail: reject,
+    })
+  })
 }
 
 /**
